0a19071272c4e01616868af1407af7df6cd3dfb6
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-09 20:49:28 +0530

    layout

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,16 @@ export default function Home() {
 return (
 
   <Layout>
-      <div className='text-blue-900'>
+      <div className='text-blue-900 flex justify-between m-2'>
 
-        Hello {session?.user?.name}
-
-        <img src={session.user.image}></img>
+        <h2>Hello {session?.user?.name}</h2>
+        
+        <div className='flex bg-gray-300 gap-1 rounded-lg'>
+        
+          <img src={session.user.image} className="w-6 h-6"/>
+          <span className='px-2'>{session?.user?.name}</span>
+        </div>
+        
 
       </div>
     </Layout>
