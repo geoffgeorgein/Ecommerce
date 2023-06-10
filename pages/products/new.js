5ee537fd92dcdce96ad1542976cce2099b07aef8
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-10 17:47:43 +0530

    products

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,16 +1,23 @@
 import Layout from '@/Components/layout';
-import React from 'react'
+import React, { useState } from 'react'
 
 const NewProduct = () => {
+
+    const [title,setTitle]=useState('');
+    const [description,setDescription]=useState('');
+    const [price,setPrice]=useState('');
+
+
   return (
     <Layout>
         <h1 className='text-blue-900 mb-2 text-lg' >New Product </h1>
         <label>Product name</label>
-        <input type='text' placeholder='product name'></input>
+        <input type='text' placeholder='product name' value={title} onChange={ev=>setTitle(ev.target.value)} ></input>
         <label>Description</label>
-        <textarea placeholder='description'></textarea>
+        <textarea placeholder='description' value={description} onChange={ev=>setDescription(ev.target.value)}></textarea>
         <label>Price (in USD)</label>
-        <input ype='text' placeholder='price'></input>
+        <input ype='number' placeholder='price' value={price} onChange={ev=>setPrice(ev.target.value)}></input>
+        <button className='btn-primary'>Save</button>
     </Layout>
   )
 }
