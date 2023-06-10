fa19a42cc23edd9587c22f3419b4463279491e11
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-10 15:14:41 +0530

    products

--- pages/products/new.js
+++ pages/products/new.js
@@ -4,8 +4,13 @@ import React from 'react'
 const NewProduct = () => {
   return (
     <Layout>
+        <h1 className='text-blue-900 mb-2 text-lg' >New Product </h1>
+        <label>Product name</label>
         <input type='text' placeholder='product name'></input>
+        <label>Description</label>
         <textarea placeholder='description'></textarea>
+        <label>Price (in USD)</label>
+        <input ype='text' placeholder='price'></input>
     </Layout>
   )
 }
