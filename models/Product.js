cb7964e0a22b8b1c9250db6b3176af72fb0df43d
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-10 21:47:19 +0530

    product

---
+++ models/Product.js
@@ -0,0 +1,13 @@
+import { Schema, model, models } from "mongoose";
+
+const ProductSchema=new Schema({
+    title:String,
+    description:String,
+    price:{type:Number,required:true},
+
+})
+
+
+
+export const Product = models.Product || model('Product', ProductSchema);
+
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,5 +1,6 @@
 import Layout from '@/Components/layout';
 import React, { useState } from 'react'
+import axios from 'axios';
 
 const NewProduct = () => {
 
@@ -7,17 +8,25 @@ const NewProduct = () => {
     const [description,setDescription]=useState('');
     const [price,setPrice]=useState('');
 
+    async function createProduct(e){
+        e.preventDefault();
+        const data={title,description,price};
+        await axios.post('/api/products',data);
+    }
+
 
   return (
     <Layout>
-        <h1 className='text-blue-900 mb-2 text-lg' >New Product </h1>
-        <label>Product name</label>
-        <input type='text' placeholder='product name' value={title} onChange={ev=>setTitle(ev.target.value)} ></input>
-        <label>Description</label>
-        <textarea placeholder='description' value={description} onChange={ev=>setDescription(ev.target.value)}></textarea>
-        <label>Price (in USD)</label>
-        <input ype='number' placeholder='price' value={price} onChange={ev=>setPrice(ev.target.value)}></input>
-        <button className='btn-primary'>Save</button>
+        <form onSubmit={createProduct}>
+            <h1 className='text-blue-900 mb-2 text-lg' >New Product </h1>
+            <label>Product name</label>
+            <input type='text' placeholder='product name' value={title} onChange={ev=>setTitle(ev.target.value)} ></input>
+            <label>Description</label>
+            <textarea placeholder='description' value={description} onChange={ev=>setDescription(ev.target.value)}></textarea>
+            <label>Price (in USD)</label>
+            <input ype='number' placeholder='price' value={price} onChange={ev=>setPrice(ev.target.value)}></input>
+            <button className='btn-primary' type='submit'>Save</button>
+        </form>
     </Layout>
   )
 }
