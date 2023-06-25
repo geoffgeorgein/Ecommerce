6bf124779bb0f5b51c0549962f3a54dd932b7af7
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-25 12:01:05 +0530

    categories

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,9 +1,18 @@
+import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/Category";
+
 
 export default async function handle(req,res){
 
     const {method}=req;
+
+    await mongooseConnect();
     console.log("API Cat")
+
+    if (method === 'GET') {
+        res.json(await Category.find());
+      }
+    
 
     if(method==='POST'){
         const {name}=req.body;
--- pages/categories.js
+++ pages/categories.js
@@ -1,10 +1,23 @@
 import Layout from '@/Components/layout';
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Categories = () => {
 
   const[name,setname]=useState('');
+  const[categories,setcategories]=useState([]);
+
+  useEffect(()=>{
+
+    fetchCategories();
+  },[]);
+
+  function fetchCategories() {
+    axios.get('/api/categories').then(result => {
+      setcategories(result.data);
+      console.log(categories);
+    });
+  }
 
   async function saveCategory(ev){
 
@@ -21,6 +34,23 @@ const Categories = () => {
             <button className='btn btn-primary'  > Save</button>
         
         </form>
+
+        <table className='basic mt-4'>
+          <thead>
+            <tr>
+              <td>Category name</td>
+            </tr>
+          </thead>
+          <tbody>
+            {categories.length>0 && categories.map(
+              category=>(
+                <tr>
+                  <td>{category.name} </td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
     </Layout>
   )
 }
